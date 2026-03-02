1a5291dddfb75903148cbae8b18113a68decfd2d
thomas carugati <carugatithomas520@gmail.com>
2026-03-02 16:06:30 -0300

    Antes de cambiar la paginación

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 const pilotosRoutes = require('./src/routes/pilotos');
 require('./src/database/db');
@@ -17,15 +18,50 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'src/views'));
 
-// rutas
+// --- RUTAS ---
+
+// 1. Inicio
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+// 2. El Piloto (Usa el archivo de rutas para el CRUD)
 app.use('/pilotos', pilotosRoutes);
 
+// 3. Resultados
+app.get('/resultados', (req, res) => {
+    res.render('resultados'); 
+});
+
+// 4. Contacto
 app.get('/contacto', (req, res) => {
   res.render('contacto');
+});
+
+// POST para el envío de MAIL
+app.post('/contacto', async (req, res) => {
+    const { nombre, email, mensaje } = req.body;
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        auth: {
+            user: 'mckenna.mcclure22@ethereal.email',
+            pass: 'S8K8V8X8V8X8V8X8V8'
+        }
+    });
+
+    try {
+        await transporter.sendMail({
+            from: email,
+            to: "tu-mail@ejemplo.com",
+            subject: `Nuevo mensaje de ${nombre}`,
+            text: mensaje
+        });
+        res.send('<h1>¡Mensaje enviado con éxito!</h1><a href="/">Volver al inicio</a>');
+    } catch (error) {
+        res.send('Error al enviar el mensaje.');
+    }
 });
 
 // servidor
